f5f4587f222b4b8237a3f0a60def2f7d0fda6429
EduardoCh93 <56979475+EduardoCh93@users.noreply.github.com>
2022-11-16 15:41:01 -0300

    Add test and document swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -51,6 +51,22 @@ router.post("/users", (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+/**
+ * @swagger
+ * /api/users:
+ *  get:
+ *    summary: return all users
+ *    tags: [User]
+ *    responses:
+ *      200:
+ *        description: new user created!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#components/schemas/User'
+*/
 // get all users
 router.get("/users", (req, res) => {
   userSchema
@@ -59,6 +75,30 @@ router.get("/users", (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+/**
+ * @swagger
+ * /api/users/{id}:
+ *  get:
+ *    summary: return a user
+ *    tags: [User]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: the user id
+ *    responses:
+ *      200:
+ *        description: a user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: '#components/schemas/User'
+ *      404:
+ *        description: user not found
+ */
 // get a user
 router.get("/users/:id", (req, res) => {
   const { id } = req.params;
@@ -68,6 +108,25 @@ router.get("/users/:id", (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+/**
+ * @swagger
+ * /api/users/{id}:
+ *  delete:
+ *    summary: delete a user
+ *    tags: [User]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: the user id
+ *    responses:
+ *      200:
+ *        description: user delete
+ *      404:
+ *        description: user not found
+ */
 // delete a user
 router.delete("/users/:id", (req, res) => {
   const { id } = req.params;
@@ -77,6 +136,32 @@ router.delete("/users/:id", (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+/**
+ * @swagger
+ * /api/users/{id}:
+ *  put:
+ *    summary: update a user
+ *    tags: [User]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: the user id
+ *    requestBody:
+ *      required: true
+ *      content: 
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: user update
+ *      404:
+ *        description: user not found
+ */
 // update a user
 router.put("/users/:id", (req, res) => {
   const { id } = req.params;
