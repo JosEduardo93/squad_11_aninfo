84b64f95f6e35913293c455fca8bf9cbe02268c6
EduardoCh93 <56979475+EduardoCh93@users.noreply.github.com>
2022-11-17 15:50:46 -0300

    update routes
    Co-authored-by: AlanCristianGoyzueta <alanlata@gmail.com>
    Co-authored-by: kamalhosain <kamalhosain@users.noreply.github.com>
    Co-authored-by: Manuel Alvarez <jmalvarez97@users.noreply.github.com>

--- src/controller/projectController.js
+++ src/controller/projectController.js
@@ -16,7 +16,38 @@ const postProject = (req, res) => {
       .catch((error) => res.json({ message: error }))
 };
 
+const getById = (req, res) => {
+    const { id } = req.params;
+    projectSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+}  
+
+const deleteById = (req, res) => {
+    const { id } = req.params;
+    projectSchema
+      .remove({ _id: id })
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  }
+
+const updateDate = (req, res) => {
+    const { id } = req.params;
+    const initDate = Date.parse(req.body.initDate);
+    const endDate = Date.parse(req.body.endDate);
+    if (initDate & endDate) {
+        projectSchema
+            .updateOne({ _id: id}, { $set: { initDate, endDate }})
+            .then((data) => res.json(data))
+            .catch((error) => res.json({ message: error}))
+    }
+}
+
 module.exports  = {
     postProject,
-    getAllProject
+    getAllProject,
+    getById,
+    deleteById,
+    updateDate
 };
--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -32,7 +32,6 @@ const router = express.Router();
  *          description: end date project
  *          
  */
-router.get("/projects", projectController.getAllProject)
 
 /**
  * @swagger
@@ -54,5 +53,81 @@ router.get("/projects", projectController.getAllProject)
  *        description: failed created
  */
 router.post("/projects", projectController.postProject);
+
+/**
+ * @swagger
+ * /api/projects:
+ *  get:
+ *   summary: get all projects
+ *   tags: [Projects]
+ *   responses:
+ *      200:
+ *        description: new user created!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#components/schemas/Project'
+ */
+router.get("/projects", projectController.getAllProject)
+
+/**
+ * @swagger
+ * /api/projects/{id}:
+ *  get:
+ *    summary: find project
+ *    tags: [Projects]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        require: true
+ *        description: the project id
+ *    responses:
+ *      200:
+ *          description: a project
+ *          content: 
+ *            application/json:
+ *              type: object
+ *              $ref: '#components/schemas/Project'
+*/
+router.get("/projects/:id", projectController.getById);
+
+router.delete("/projects/:id", projectController.deleteById);
+
+/**
+ * @swagger
+ * /api/projects/{id}/dates:
+ *  put:
+ *    summary: update a user
+ *    tags: [Projects]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: the user id
+ *    requestBody:
+ *      required: true
+ *      content: 
+ *        application/json:
+ *          schema: 
+ *              type: object
+ *              properties:
+ *                  initDate: 
+ *                      type: string
+ *                  endDate: 
+ *                      type: string
+ *    responses:
+ *      200:
+ *        description: user update
+ *      404:
+ *        description: user not found
+ */
+router.put("/projects/:id/dates", projectController.updateDate);
+// router.put("/projects/:id/date/:endDate", projectController.updateEndDate);
 
 module.exports = router
