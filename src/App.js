d90856dd5f80226095642901dda21d8ee19ce638
jmalvarez97 <jualvarez@fi.uba.ar>
2022-11-30 21:38:56 -0300

    add whitelist

--- src/App.js
+++ src/App.js
@@ -17,9 +17,29 @@ app.get("/", (req, res) => {
 }); 
 
 // midleware
-app.use(cors({
-    origin:"*"
-}));
+
+var cors = require('cors'); //import cors module
+
+var whitelist = ['https://squad11-proyectos.onrender.com', 'http://localhost:8080', 'https://project-s11.onrender.com']; //white list consumers
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+};
+
+app.use(cors(corsOptions)); //adding cors middleware to the express with above configurations
+
+
+
+
 app.use(express.json());
 app.use('/api', projectRouter);
 app.use('/api', taskRouter);
